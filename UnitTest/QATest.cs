85b658fa93cec7775fbd48a0e80d8923201cee13
Tit-tytant <saurabh.tomar@gmail.com>
2017-03-04 12:42:09 -0700

    Added Unit Test foundations

---
+++ UnitTest/QATest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest {
+    [TestClass]
+    public class QATest {
+        [TestMethod]
+        public bool TestMethod1() {
+            return true;
+        }
+    }
+}
